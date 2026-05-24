2b84fdaf2a57104c41bbde786f0a3904d79d3744
arena672 <ydvlaxmi1246@gmail.com>
2026-05-24 15:18:47 +0530

    Add screenshots and ER diagram

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4832bd167a3a4477b9db6d3e86bf4659cd060a52")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22430c22d825dccc74e90f2372030c69f7a741a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
